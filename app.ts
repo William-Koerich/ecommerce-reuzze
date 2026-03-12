8ef5c1f36f72892906fc2a6b248ac2a5a10d487c
William Koerich <williamkoerich17@gmail.com>
2026-03-12 18:27:35 -0300

    fix: fixed cors

--- app.ts
+++ app.ts
@@ -1,7 +1,12 @@
 import express from "express"
 import routes from "./src/routes/index"
+import cors from "cors"
+
 
 const app = express()
+
+
+app.use(cors())
 
 app.use(express.json())
 
--- server.ts
+++ server.ts
@@ -1,10 +1,5 @@
 import app from "./app"
 
-import cors from "cors"
-
-app.use(cors())
-
-app.use(cors())
 app.listen(3333, () => {
   console.log("🚀 Server running on port 3333")
 })
