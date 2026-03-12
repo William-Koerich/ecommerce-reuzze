7fb890d86ced06ddf39c165f4fdae68bb21a85c9
William Koerich <williamkoerich17@gmail.com>
2026-03-12 10:17:18 -0300

    feat: created all endpoints of products

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,85 @@
+import { Request, Response } from "express"
+import { ProductService } from "../services/product.service"
+
+const productService = new ProductService()
+
+export class ProductController {
+
+  async create(req: Request, res: Response) {
+    try {
+
+      const product = await productService.create(req.body)
+
+      return res.status(201).json(product)
+
+    } catch (error: any) {
+
+      return res.status(400).json({
+        message: error.message
+      })
+
+    }
+  }
+
+  async findAll(req: Request, res: Response) {
+
+    const products = await productService.findAll()
+
+    return res.json(products)
+  }
+
+  async findById(req: Request<{ id: string }>, res: Response) {
+    try {
+
+      const { id } = req.params
+
+      const product = await productService.findById(id)
+
+      return res.json(product)
+
+    } catch (error: any) {
+
+      return res.status(404).json({
+        message: error.message
+      })
+
+    }
+  }
+
+  async update(req: Request<{ id: string }>, res: Response) {
+    try {
+
+      const { id } = req.params
+
+      const product = await productService.update(id, req.body)
+
+      return res.json(product)
+
+    } catch (error: any) {
+
+      return res.status(400).json({
+        message: error.message
+      })
+
+    }
+  }
+
+  async delete(req: Request<{ id: string }>, res: Response) {
+    try {
+
+      const { id } = req.params
+
+      const result = await productService.delete(id)
+
+      return res.json(result)
+
+    } catch (error: any) {
+
+      return res.status(400).json({
+        message: error.message
+      })
+
+    }
+  }
+
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,18 @@
 import { Router } from "express"
 import { UserController } from "../controllers/user.controller"
+import { ProductController } from "../controllers/product.controller"
 
 const router = Router()
+
 const userController = new UserController()
+const productController = new ProductController()
 
 router.post("/users", userController.create)
+
+router.post("/products", productController.create)
+router.get("/products", productController.findAll)
+router.get("/products/:id", productController.findById)
+router.put("/products/:id", productController.update)
+router.delete("/products/:id", productController.delete)
 
 export default router
