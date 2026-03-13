278439342e8d604b7462cdbd66b9ef4daad2cc65
William Koerich <williamkoerich17@gmail.com>
2026-03-13 18:10:46 -0300

    fix: fixed app.ts

--- app.ts
+++ app.ts
@@ -1,11 +1,14 @@
 import express from "express"
 import routes from "./src/routes/index"
 import cors from "cors"
+import path from "path"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use("/uploads", express.static(path.resolve("uploads")))
 
 app.use("/api", routes)
 
